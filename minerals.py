a973f70ec5fd2d7e336fba0f6465c33f76b980c1
Lia Corrales <lia@astro.wisc.edu>
2018-06-23 15:53:28 +0200

    wrote unit_mass property

--- minerals.py
+++ minerals.py
@@ -6,12 +6,45 @@
 #-------------------------------------------------------------------------
 
 from molecules import Molecule
+from abundances import amu
 import astropy.constants as c
 
 class Mineral(Molecule):
     """
     Subclass of molecules.Molecule because it requires some of the same
     functionality.
+
+    Inputs
+    ------
+        name : string (optional)
+            A string describing the molecule. Defaults to None
+
+        composition : dictionary
+            A dictionary describing the number of atoms of each element type
+            in the molecule
+
+    Attributes
+    ----------
+    name : string (or None)
+
+    composition : dictionary
+
+    weight_amu : float
+        The weight of the molecule in atomic mass units (amu)
+
+    weight : astropy.units.Quantity
+        The mass of the molecule
+
+    elements : list
+        Returns the elements that make up the keys for self.composition
+
+    unit_mass : float
+        Returns the mass of the mineral unit cell
     """
     def __init__(self, composition, name=None):
         Molecule.__init__(self, composition, name=name)
+
+    @property
+    def unit_mass(self):
+        return sum([self.number(ele) * amu[ele] * c.u.to('g')
+            for ele in self.elements])
